b1ef87af6be635f0f18029b7fbe0d752a7d7b5ae
jonginson <jongin718@gmail.com>
2023-10-13 16:16:56 +0900

    #25 - 해시태그검색기능구현
    해시태그 뷰에 모든 게시글에서 나오는 해시태그를
    정리해서 보여주는 기능을 넣기 위해,
    해시태그만 뽑아서 unique 하게 보여주는 쿼리를
    querydsl로 작성함
    그 외 서비스 코드, 컨트롤러 작성

--- board-project/src/main/java/com/project/boardproject/repository/ArticleRepository.java
+++ board-project/src/main/java/com/project/boardproject/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.project.boardproject.repository;
 
 import com.project.boardproject.domain.Article;
 import com.project.boardproject.domain.QArticle;
+import com.project.boardproject.repository.querydsl.ArticleRepositoryCustom;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.domain.Page;
@@ -15,6 +16,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
+        ArticleRepositoryCustom,
         QuerydslPredicateExecutor<Article>,
         QuerydslBinderCustomizer<QArticle> {
 
--- board-project/src/main/java/com/project/boardproject/service/ArticleService.java
+++ board-project/src/main/java/com/project/boardproject/service/ArticleService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -63,4 +64,17 @@ public class ArticleService {  // command+shift+t를 누르면 동일한 위치�
         articleRepository.deleteById(articleId);
     }
 
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticlesViaHashtag(String hashtag, Pageable pageable) {
+        if (hashtag == null || hashtag.isBlank()) {
+            return Page.empty(pageable);
+        }
+
+        return articleRepository.findByHashtag(hashtag, pageable).map(ArticleDto::from);
+    }
+
+    public List<String> getHashtags() {
+        return articleRepository.findAllDistinctHashtags();
+    }
 }
