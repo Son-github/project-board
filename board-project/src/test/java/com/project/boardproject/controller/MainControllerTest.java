e5b3c92457469679a3163950b4e05bf5b4ee2bdc
jonginson <jongin718@gmail.com>
2023-10-20 13:45:42 +0900

    ** 오류 수정 **

--- board-project/src/test/java/com/project/boardproject/controller/MainControllerTest.java
+++ board-project/src/test/java/com/project/boardproject/controller/MainControllerTest.java
@@ -1,6 +1,8 @@
 package com.project.boardproject.controller;
 
-import com.project.boardproject.config.SecurityConfig;
+
+import com.project.boardproject.config.TestSecurityConfig;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,20 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View RootController")
-@Import(SecurityConfig.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest(MainController.class)
 class MainControllerTest {
 
     private final MockMvc mvc;
 
     MainControllerTest(@Autowired MockMvc mvc) {this.mvc = mvc;} // Autowired?
-
 
 
     @Disabled
