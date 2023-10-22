03cbfbd491a1c01d09ac6c6676d3bff4596950c2
jonginson <jongin718@gmail.com>
2023-10-22 21:18:48 +0900

    #42-댓글기능고도화(response)
    'Response'파일 수정

--- board-project/src/main/java/com/project/boardproject/dto/response/ArticleCommentResponse.java
+++ board-project/src/main/java/com/project/boardproject/dto/response/ArticleCommentResponse.java
@@ -3,6 +3,9 @@ package com.project.boardproject.dto.response;
 import com.project.boardproject.dto.ArticleCommentDto;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public record ArticleCommentResponse(
         Long id,
@@ -10,27 +13,40 @@ public record ArticleCommentResponse(
         LocalDateTime createdAt,
         String email,
         String nickname,
-        String userId
+        String userId,
+        Long parentCommentId,
+        Set<ArticleCommentResponse> childComments
 ) {
 
     public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId) {
-        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId);
+        return ArticleCommentResponse.of(id, content, createdAt, email, nickname, userId, null);
     }
 
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId, Long parentCommentId) {
+        Comparator<ArticleCommentResponse> childCommentComparator = Comparator // what? 정렬 기준을 정해줌
+                .comparing(ArticleCommentResponse::createdAt)
+                .thenComparingLong(ArticleCommentResponse::id);
+        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId, parentCommentId, new TreeSet<>(childCommentComparator)); // TreeSet은 자동 정렬 Set
+    }
     public static ArticleCommentResponse from(ArticleCommentDto dto) {
         String nickname = dto.userAccountDto().nickname();
         if (nickname == null || nickname.isBlank()) {
             nickname = dto.userAccountDto().userId();
         }
 
-        return new ArticleCommentResponse(
+        return ArticleCommentResponse.of(
                 dto.id(),
                 dto.content(),
                 dto.createdAt(),
                 dto.userAccountDto().email(),
                 nickname,
-                dto.userAccountDto().userId()
+                dto.userAccountDto().userId(),
+                dto.parentCommentId()
         );
+    }
+
+    public boolean hasParentComment() {
+        return parentCommentId != null;
     }
 
 }
